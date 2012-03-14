3ac99ea03bd0fc2321d67c8553646a63216f0202
Gary Bernhardt <gary.bernhardt@gmail.com>
2012-03-14 10:17:52 -0700

    add scenario: Search for cached term

--- features/search.feature
+++ features/search.feature
@@ -6,6 +6,14 @@ Feature: Users can learn how good something is
     And I search for the beatles
     Then the beatles should have a higher score than comcast
 
+  Scenario: Search for cached term
+    Given microsoft is cached with a score of 2.5
+    When I search for microsoft
+    Then I should see a score of 2.5
+
+  Scenario: Search for term with no score
+    When pending
+
   Scenario: Why are the results so extreme for the beatles and comcast?
     When pending
 
--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,9 +1,17 @@
+Given /^microsoft is cached with a score of 2.5$/ do
+  CachedScore.save_score("microsoft", 2.5)
+end
+
 When /^I search for (.*)$/ do |term|
   @scores ||= {}
-  @scores[term] = ScoreCache.for_term(term)
+  @scores[term] = @last_score = ScoreCache.for_term(term)
 end
 
 Then /^the beatles should have a higher score than comcast/ do
   @scores["the beatles"].should be > @scores["comcast"]
+end
+
+Then /^I should see a score of 2.5$/ do
+  @last_score.should == 2.5
 end
 
