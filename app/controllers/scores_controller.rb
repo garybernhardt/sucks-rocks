0cfb98e73a26c85f068c15b269e9b79902c8ebdc
Gary Bernhardt <gary.bernhardt@gmail.com>
2012-03-19 15:55:22 -0700

    fix bug: NoScore wasn't converted to nil in JSON

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,6 +2,7 @@ class ScoresController < ApplicationController
   def show
     term = params[:term]
     score = ScoreCache.for_term(term)
+    score = nil if score == RockScore::NoScore
     render :json => {:term => term, :score => score}
   end
 end
--- spec/controllers/scores_controller_spec.rb
+++ spec/controllers/scores_controller_spec.rb
@@ -6,5 +6,11 @@ describe ScoresController do
     get :show, :term => "microsoft"
     response.body.should == {:term => "microsoft", :score => 8.5}.to_json
   end
+
+  it "returns nil for NoScore" do
+    ScoreCache.stub(:for_term).with("microsoft") { RockScore::NoScore }
+    get :show, :term => "microsoft"
+    response.body.should == {:term => "microsoft", :score => nil}.to_json
+  end
 end
 
